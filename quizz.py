592b9a5541c30a0c8a096d6acbb253ad57babb34
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-05-06 14:30:09 +0000

    quiz

--- quizz.py
+++ quizz.py
@@ -1,1 +1,9 @@
+print("Quiz sobre futebol")
+print("                  ")
+print("Quantos jogadores cada time tem em campo? \n A-10 \n B-11 \n C-22")
+aaas = input("")
+if aaas == "A":
+    print("fgh")
+else:
+    print("fghjs")
 
