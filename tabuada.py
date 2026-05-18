c83460c3c4b967976237efdda7b2a1240e725735
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-05-18 10:49:53 +0000

    v1

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,5 @@
-
+numero = int(input("Tabuada de qual número? "))
+print(f"--- Tabuada do {numero} ---")
+for i in range(1,11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
