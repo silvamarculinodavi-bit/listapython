cbd800b140cbbaaf01b58e59dfef20cbdc226ab8
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-05-27 13:13:50 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,8 @@
+def exibir_cabecalho():
+    print("=" * 30)
+    print("SISTEMA DE GESTÃO ESCOLAR")
+    print("Versão 1.0 - Desenvolvido no Codespace")
+    print("=" * 30)
+
+exibir_cabecalho()
+print("Menu Principal:")
