c505ed2952e75c81de601488b48c7067c24fc5ea
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-05-30 16:02:54 +0000

    v1

--- simulador_investimento_cdb.py
+++ simulador_investimento_cdb.py
@@ -1,2 +1,16 @@
 #SIMULADOR DE INVESTIMENTO CDB--
-
+print("=" * 30)
+print("SIMULADOR DE INVESTIMENTO")
+print("Versão 1.0 - Desenvolvido no Codespace")
+print("=" * 30)
+print(" ")
+aporte = float(input("Quanto você vai depositar no mês?"))
+print("Juros do CBD: 1.24")
+meses = int(input("Por quantos meses você vai investir?"))
+juros_decimal = 1.24/100
+total = 0
+for mes in range(1,meses+1):
+    total = total + aporte
+    total = total + (total*juros_decimal)
+    print(f"Mês{mes}:Saldo Total = R${total}")
+print(f"Ao final de {meses} meses,você tera o valor de R${total:2f}")
