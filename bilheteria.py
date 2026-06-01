96b93dcf90331522c133716c4f596d915fb0c100
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-06-01 10:45:35 +0000

    v1

---
+++ bilheteria.py
@@ -0,0 +1,21 @@
+#aluno1: Padronizar nome do filme
+def formatar(nome):
+    return nome.upper()
+#aluno2: Verificador de idade
+def verificador(idade):
+    if idade >= 18:
+        return "Acesso permitido"
+    else:
+        return "Acesso Negado"
+#aluno3: Mensagem de Retorno
+def gerar_mensagem(status):
+    if status == "Acesso Autorizado":
+        return "Tenha uma ótima sessão!"
+    else:
+        return "Sentimos, mas você não tem idade minima."
+#aluno4: Funçao do algoritmo
+filme_enredo = input("Digite o filme escolhido: ")
+idade_entrada = int(input("Digite sua idade: "))
+nome_final = formatar(filme_enredo)
+status_acesso = verificador(idade_entrada)
+mensagem = gerar_mensagem(status_acesso)
