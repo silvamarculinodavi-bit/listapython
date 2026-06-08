56cf6e25852506d3a68dbf3ef561fcb3f6bde640
silvamarculinodavi-bit <silvamarculinodavi@gmail.com>
2026-06-08 11:39:21 +0000

    v1

---
+++ imc.py
@@ -0,0 +1,14 @@
+#etapa1--
+def calcular_imc(imc):
+    imc = peso/(altura * 2)
+
+#etapa2--
+def teste_imc(valor_imc)
+    if valor_imc >= 25:
+        return("Procure um Médico")
+    else:
+        return("Peso Normal")
+
+#etapa3--
+def resultado_imc(mensagem)
+    if mensagem == "Procure um Médico"
